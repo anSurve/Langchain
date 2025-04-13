604f72385cc7aa67147945092e473fc8bf5330a2
aniket.surve <aniket.surve@synechron.com>
2025-04-13 19:10:48 +0530

    created env and few changes

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from dotenv import load_dotenv
+import os
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    load_dotenv()
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+    print(os.environ["COOL_API_KEY"])
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    print_hi("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
